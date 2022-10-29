bfe29540320afd6eda8a2bc7874587fc2935d25d
Irina <irina.delving@yandex.ru>
2022-10-30 01:28:25 +0300

    add trask1 and gitinit

---
+++ task1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите 2 числа");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2){
+    Console.WriteLine("Первое число больше второго");
+}
+else{
+    Console.WriteLine("Первое число меньше второго");
+}
