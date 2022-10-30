6d15c32d866d2bfe97ca2f2f5a36cdad8852201e
Irina <irina.delving@yandex.ru>
2022-10-31 00:49:19 +0300

    add task4

---
+++ task4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int num = Int32.Parse(Console.ReadLine());
+int i = 1;
+while (i <= num){
+    if (i%2==0)
+    Console.WriteLine(i);
+
+     i++;
+}
+     
+    
