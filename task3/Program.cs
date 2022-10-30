16c0c2b94ba92df9d92b04af52fcab251d976280
Irina <irina.delving@yandex.ru>
2022-10-30 22:20:17 +0300

    add task3

---
+++ task3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+string num = Console.ReadLine();
+if(Int32.Parse(num)%2==0){
+    Console.WriteLine("Число четное");
+}
+else{
+    Console.WriteLine("Число нечетное");
+}
